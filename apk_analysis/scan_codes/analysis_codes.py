31f856a97a68d3a71f69933626b633ce2188be26
AllenSun7 <allensun623@gmail.com>
2021-02-23 00:53:52 -0600

    output csv

--- apk_analysis/scan_codes/analysis_codes.py
+++ apk_analysis/scan_codes/analysis_codes.py
@@ -1,20 +1,41 @@
 from scan_codes import ScanCodes
 from collections import defaultdict
+import pandas as pd
 
 def analysis_api(apk_name, d_api, d_methods):
     # update api
-    print(d_methods)
+    # print(d_methods)
     for api, _ in d_methods.items():
         d_api[api].append(apk_name)
 
     return d_api
 
 def analysis_permissions(apk_name, d_permission, l_permission):
-    print(l_permission)
+    # update permission
+    # print(l_permission)
     for p in l_permission:
         d_permission[p].append(apk_name)
 
     return d_permission
+
+def convert_dict(l_apk_name, d_feature):
+    # convert {feature: [apk_name]} => {feature: [0, 1, ...]}
+    d_int_feature = defaultdict(list)
+    for feature, apk_names in d_feature.items():
+        for apk_name in l_apk_name:
+            d_int_feature[feature].append(int(apk_name in apk_names))
+    return d_int_feature
+
+def output_csv(d_apk_name, d_int_api, d_int_permission):
+    # concate api and permission, and output as a csv file
+    # Todo: add class(Benign 0; Malicious: 1) for each apk
+    d_apk_name.update(d_int_api)
+    d_apk_name.update(d_int_permission)
+    df_results = pd.DataFrame(data=d_apk_name)
+
+    df_results.to_csv("../db/api_permission.csv", index=False)
+    print(d_apk_name)
+
 
 def print_title(title):
     # print break line with title
@@ -26,6 +47,7 @@ def print_title(title):
 def main():
     print_title("Start Scanning...")
     l_apk_name = ["0a37f086841ff927ec8bee9f3bdb1048ecfba54b09aea400e980bd3ef301519d.77f09f214993016fbc747b5bafa5f94f", "0ac4aa9b5413666a2bd66b96faa0cc8e656144ffcd351101071f8d028970befa.2fd32776a17d88eaa6fb2bcd792695f0"]
+    d_apk_name = {"apk_name": l_apk_name}
     # store api as dictionary {key: value} {api: list of apks}
     d_api = defaultdict(list)
     # store permission as dictionary {key: value} {permission: list of apks}
@@ -36,9 +58,12 @@ def main():
         scan_codes = ScanCodes(apk_src)
         d_api = analysis_api(apk_name, d_api, scan_codes.d_methods)
         d_permission = analysis_permissions(apk_name, d_permission, scan_codes.l_permission)
-    
-    print(d_api)
-    print(d_permission)
+    # convert dict
+    d_int_api = convert_dict(l_apk_name, d_api)
+    d_int_permission = convert_dict(l_apk_name, d_permission)
+    # print(d_int_api)
+    # print(d_int_permission)
+    output_csv(d_apk_name, d_int_api, d_int_permission)
 
 if __name__ == '__main__':
     main()
