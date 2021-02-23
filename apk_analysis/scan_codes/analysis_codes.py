a9b996e265e512154a36f64fb5635f99bef61bd2
AllenSun7 <allensun623@gmail.com>
2021-02-23 00:17:45 -0600

    dictionary for api and permission

--- apk_analysis/scan_codes/analysis_codes.py
+++ apk_analysis/scan_codes/analysis_codes.py
@@ -1,11 +1,20 @@
 from scan_codes import ScanCodes
+from collections import defaultdict
 
-
-def analysis_api(d_methods):
+def analysis_api(apk_name, d_api, d_methods):
+    # update api
     print(d_methods)
+    for api, _ in d_methods.items():
+        d_api[api].append(apk_name)
 
-def analysis_permissions(l_permission):
+    return d_api
+
+def analysis_permissions(apk_name, d_permission, l_permission):
     print(l_permission)
+    for p in l_permission:
+        d_permission[p].append(apk_name)
+
+    return d_permission
 
 def print_title(title):
     # print break line with title
@@ -16,11 +25,20 @@ def print_title(title):
 
 def main():
     print_title("Start Scanning...")
-    apk_src = "../../apps/apks_codes/0a37f086841ff927ec8bee9f3bdb1048ecfba54b09aea400e980bd3ef301519d.77f09f214993016fbc747b5bafa5f94f"
-    scan_codes = ScanCodes(apk_src)
-    # analysis_api(scan_codes.d_methods)
-    analysis_permissions(scan_codes.l_permission)
-
+    l_apk_name = ["0a37f086841ff927ec8bee9f3bdb1048ecfba54b09aea400e980bd3ef301519d.77f09f214993016fbc747b5bafa5f94f", "0ac4aa9b5413666a2bd66b96faa0cc8e656144ffcd351101071f8d028970befa.2fd32776a17d88eaa6fb2bcd792695f0"]
+    # store api as dictionary {key: value} {api: list of apks}
+    d_api = defaultdict(list)
+    # store permission as dictionary {key: value} {permission: list of apks}
+    d_permission = defaultdict(list)
+    # Update dictionary of api and permission
+    for apk_name in l_apk_name:
+        apk_src = "../../apps/apks_codes/" + apk_name
+        scan_codes = ScanCodes(apk_src)
+        d_api = analysis_api(apk_name, d_api, scan_codes.d_methods)
+        d_permission = analysis_permissions(apk_name, d_permission, scan_codes.l_permission)
+    
+    print(d_api)
+    print(d_permission)
 
 if __name__ == '__main__':
     main()
--- apk_analysis/scan_codes/extract_apk.py
+++ apk_analysis/scan_codes/extract_apk.py
@@ -8,9 +8,13 @@ def run_scan(apk_src):
     scan_api = ScanCodes(apk_src)
 
 def file_dir_exist(target_path):
-    exists = path.exists(target_path)
+    exists = False
+    try:
+        exists = path.exists(target_path)
+    except Exception as e:
+        print(e)
     if not exists:
-        print("\"" + target_path + "\"" + " NOT EXISTS!")
+        print("\"" + str(target_path) + "\"" + " NOT EXISTS!")
     return exists
 
 def check_apk(target_path):
@@ -31,9 +35,9 @@ def print_title(title):
 def main():
     # zip_src = "../apps/apks_upzip/0a37f086841ff927ec8bee9f3bdb1048ecfba54b09aea400e980bd3ef301519d.77f09f214993016fbc747b5bafa5f94f.zip"
     # input source apk
-    apk_src = "../../apps/apks/0a37f086841ff927ec8bee9f3bdb1048ecfba54b09aea400e980bd3ef301519d.77f09f214993016fbc747b5bafa5f94f.apk" 
+    apk_src = "../../apps/apks/0ac4aa9b5413666a2bd66b96faa0cc8e656144ffcd351101071f8d028970befa.2fd32776a17d88eaa6fb2bcd792695f0.apk" 
     # output source code
-    apk_dst = "../../apps/apks_codes/0a37f086841ff927ec8bee9f3bdb1048ecfba54b09aea400e980bd3ef301519d.77f09f214993016fbc747b5bafa5f94f"
+    apk_dst = "../../apps/apks_codes/0ac4aa9b5413666a2bd66b96faa0cc8e656144ffcd351101071f8d028970befa.2fd32776a17d88eaa6fb2bcd792695f0"
     decompile_apk(apk_src, apk_dst)
     # check dir and apk file
     if file_dir_exist(apk_dst) and check_apk(apk_src):
